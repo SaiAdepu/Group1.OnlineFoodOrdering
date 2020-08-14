74d5cb172d988ebf326ba822a7815717a2358e63
rupireddy <spandanarupireddy@gmail.com>
2020-08-14 20:58:43 +0530

    hello

--- Znalytics.Group1.OnlineFoodOrdering/Program.cs
+++ Znalytics.Group1.OnlineFoodOrdering/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Znalytics.Group1.OnlineFoodOrdering
 {
     class Program
     {
-        static void Main(string[] args)
-            mmdlmdmsmmlsm
+        static void Main()
         {
+            System.Console.WriteLine("Hello");
+
         }
     }
 }
