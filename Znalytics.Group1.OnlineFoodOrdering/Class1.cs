e6007ee79bccf1defe160a4825932fb637fd46f1
Pavanbaswapuram <basavapurampavan6@gmail.com>
2020-08-15 09:50:58 +0530

    pavan employee login

---
+++ Znalytics.Group1.OnlineFoodOrdering/Class1.cs
@@ -0,0 +1,68 @@
+﻿/// <summary>
+/// Represents Food Delivery Employeelogin
+/// </summary>
+public class Employeelogin
+{
+    private int _employeeID;
+    private string _password;
+    private string _employeeName;
+    private long _mobileNumber;
+    private string _foodStoreID;
+    /// <summary>
+    /// employeeID details
+    /// </summary>
+    public void SetEmployeeID(int value)
+    {
+        _employeeID = value;
+    }
+
+    public int GetEmployeeID()
+    {
+        return _employeeID;
+    }
+    /// <summary>
+    /// Password details 
+    /// </summary>
+    public void SetPassword(string value)
+    {
+        _password = value;
+    }
+    public string GetPassword()
+    {
+        return _password;
+    }
+    /// <summary>
+    /// EmployeeName details
+    /// </summary>
+    public void SetEmployeeName(string value)
+    {
+        _employeeName = value;
+    }
+    public string GetEmployeeName()
+    {
+        return _employeeName;
+    }
+    /// <summary>
+    /// Mobilenumber details
+    /// </summary>
+    public void SetMobileNumber(long value)
+    {
+        _mobileNumber = value;
+    }
+    public long GetMobileNumber()
+    {
+        return _mobileNumber;
+    }
+    /// <summary>
+    /// FoodStorageid Details
+    /// </summary>
+    public void SetFoodStoreID(string value)
+    {
+        _foodStoreID = value;
+    }
+    public string GetFoodStoreID()
+    {
+        return _foodStoreID;
+    }
+}
+
--- Znalytics.Group1.OnlineFoodOrdering/Program.cs
+++ Znalytics.Group1.OnlineFoodOrdering/Program.cs
@@ -1,13 +1,33 @@
-﻿
-
-namespace Znalytics.Group1.OnlineFoodOrdering
+﻿class Program10
 {
-    class Program
+    static void Main()
     {
-        static void Main()
-        {
-            System.Console.WriteLine("hii");
+        Employeelogin f = new Employeelogin();
 
-        }
+        System.Console.WriteLine("enter the employeelogin details");
+
+        System.Console.WriteLine("enter the employeeID");
+        f.SetEmployeeID(int.Parse(System.Console.ReadLine()));
+
+        System.Console.WriteLine("enter the Password");
+        f.SetPassword(System.Console.ReadLine());
+
+        System.Console.WriteLine("enter the EmployeeName");
+        f.SetEmployeeName(System.Console.ReadLine());
+
+        System.Console.WriteLine("enter the MobileNumber");
+        f.SetMobileNumber(System.Convert.ToInt64(System.Console.ReadLine()));
+
+        System.Console.WriteLine("enter the Food storeid ");
+        f.SetFoodStoreID(System.Console.ReadLine());
+        ///enter the details
+        System.Console.WriteLine("enter the EmployeeName" + f.GetEmployeeID());
+        System.Console.WriteLine("enter the Password" + f.GetPassword());
+        System.Console.WriteLine("enter the EmployeeName" + f.GetEmployeeName());
+        System.Console.WriteLine("enter the MobileNumber" + f.GetMobileNumber());
+        System.Console.WriteLine("enter the Food storeid " + f.GetFoodStoreID());
+
     }
 }
+
+
